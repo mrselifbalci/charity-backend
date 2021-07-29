e604f85debdf807b1e58bb770f2960d9dacafb06
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-07-29 17:07:51 +0100

    added filter controller to emaillist

--- controllers/emaillist.controllers.js
+++ controllers/emaillist.controllers.js
@@ -15,3 +15,54 @@ exports.getAll = async (req, res) => {
 		res.json({ status: 404, error });
 	}
 };
+
+exports.getWithQuery = async (req, res) => {
+	try {
+		const query =
+			typeof req.body.query === 'string'
+				? JSON.parse(req.body.query)
+				: req.body.query;
+		const { page, limit } = req.query;
+		const response = await emailListModel
+			.find(query)
+			.limit(limit * 1)
+			.skip((page - 1) * limit)
+			.sort({ createdAt: -1 });
+		const total = await response.find(query).countDocuments();
+		const pages = limit === undefined ? 1 : Math.ceil(total / limit);
+		res.json({
+			message: 'Filtered email list',
+			total,
+			pages,
+			status: 200,
+			response,
+		});
+	} catch (error) {
+		res.json({ status: 404, message: error });
+	}
+};
+
+exports.create = async (req, res) => {
+	const { type, email, firstname, lastname, message, isActive, isDeleted } = req.body;
+
+	const newEmailList = await new emailListModel({
+		type,
+		email,
+		firstname,
+		lastname,
+		message,
+		isActive,
+		isDeleted,
+	});
+
+	newEmailList
+		.save()
+		.then((response) =>
+			res.json({
+				status: 200,
+				message: 'New email list is created successfully',
+				response,
+			})
+		)
+		.catch((err) => res.json({ status: 404, message: err }));
+};
--- routes/emaillist.routes.js
+++ routes/emaillist.routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const emailListControllers = require('../controllers/emaillist.controllers');
 
 router.get('/emaillist', emailListControllers.getAll);
+router.post('/emaillist', emailListControllers.create);
+router.post('/emaillist/filter', emailListControllers.getWithQuery);
 
 module.exports = router;
