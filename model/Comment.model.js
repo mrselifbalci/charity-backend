1275893855f22b4612afc82adaf105b08ccd8b30
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-08-13 21:43:53 +0100

    updated comment model

--- model/Comment.model.js
+++ model/Comment.model.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 const CommentsSchema = new Schema(
 	{
 		userId: { type: Schema.Types.ObjectId, ref: 'user' },
-		title: { type: String,required:true },
-		content: { type: String},
+		title: { type: String, required: true },
+		content: String,
 		listId: { type: Schema.Types.ObjectId, ref: 'list' },
 		isActive: { type: Boolean, default: true },
-		reasonToBlock:{ type: String, default: '' },
-		isDeleted: { type: Boolean, default: false }, 
+		reasonToBlock: { type: String, default: '' },
+		isDeleted: { type: Boolean, default: false },
 	},
 	{ timestamps: true }
 );
