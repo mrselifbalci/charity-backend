a3ad442569546c563e780b716bae27958b01832a
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-08-16 23:01:06 +0100

    updated messages model

--- model/Messages.model.js
+++ model/Messages.model.js
@@ -5,14 +5,11 @@ const MessagesSchema = new Schema(
 	{
 		firstname: { type: String, required: true },
 		lastname: { type: String, required: true },
-		subject: { type: String, required: true },
-		content: { type: String, required: true },
+		message: { type: String, required: true },
 		email: { type: String, required: true },
-		phoneNumber: String,
 		isActive: { type: Boolean, default: true },
 		isRead: { type: Boolean, default: false },
 		isDeleted: { type: Boolean, default: false },
-		isReplied: { type: Boolean, default: false },
 	},
 	{ timestamps: true }
 );
