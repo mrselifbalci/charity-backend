cf5217e2f8dd3aca68aefd00255cba9b4145a93d
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-08-21 23:40:34 +0100

    updated slider model

--- model/Slider.model.js
+++ model/Slider.model.js
@@ -3,17 +3,16 @@ const Schema = mongoose.Schema;
 
 const SliderSchema = new Schema(
 	{
-        type:String,
-		title: { type: String },
-        mediaId: { type: mongoose.Types.ObjectId, ref: 'media' },
-        quote:String,
-        quoteAuthor:String,
-        quoteAuthorMedia: { type: mongoose.Types.ObjectId, ref: 'media' },
+		type: String,
+		title: String,
+		mediaId: { type: mongoose.Types.ObjectId, ref: 'media' },
+		quote: String,
+		quoteAuthor: String,
+		quoteAuthorMedia: { type: mongoose.Types.ObjectId, ref: 'media' },
 		isActive: { type: Boolean, default: true },
 		isDeleted: { type: Boolean, default: false },
-		
-	}, 
- 
+	},
+
 	{ timestamps: true }
 );
 
